8e88d374fb76f3f2f52ea1c75b24253977aa0906
Thundersmn <563261107@qq.com>
2017-04-15 00:25:54 +1000

    test @Stone

--- src/com/unimelb/swen30006/metromadness/passengers/Passenger.java
+++ src/com/unimelb/swen30006/metromadness/passengers/Passenger.java
@@ -1,20 +1,21 @@
 package com.unimelb.swen30006.metromadness.passengers;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import com.unimelb.swen30006.metromadness.stations.Station;
 
 public class Passenger {
 
-	final public int id;
-	public Station beginning;
-	public Station destination;
-	public float travelTime;
-	public boolean reachedDestination;
-	public Cargo cargo;
 	
-	public Passenger(int id, Random random, Station start, Station end){
+	
+	final public int id;
+	private Station beginning;
+	private Station destination;
+	private float travelTime;
+	private boolean reachedDestination;
+	private Cargo cargo;
+
+	public Passenger(int id, Random random, Station start, Station end) {
 		this.id = id;
 		this.beginning = start;
 		this.destination = end;
@@ -22,23 +23,25 @@ public class Passenger {
 		this.travelTime = 0;
 		this.cargo = generateCargo(random);
 	}
-	
-	public void update(float time){
-		if(!this.reachedDestination){
+
+	public void update(float time) {
+		if (!this.reachedDestination) {
 			this.travelTime += time;
 		}
 	}
-	public Cargo getCargo(){
+
+	public Cargo getCargo() {
 		return cargo;
 	}
-	public Cargo generateCargo(Random random){
+
+	public Cargo generateCargo(Random random) {
 		return new Cargo(random.nextInt(51));
 	}
-	
-	public class Cargo{
+
+	public class Cargo {
 		private int weight;
-		
-		public Cargo(int weight){
+
+		public Cargo(int weight) {
 			this.setWeight(weight);
 		}
 
@@ -51,6 +54,44 @@ public class Passenger {
 		}
 	}
 
-	
-	
+	public Station getBeginning() {
+		return beginning;
+	}
+
+	public void setBeginning(Station beginning) {
+		this.beginning = beginning;
+	}
+
+	public Station getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Station destination) {
+		this.destination = destination;
+	}
+
+	public float getTravelTime() {
+		return travelTime;
+	}
+
+	public void setTravelTime(float travelTime) {
+		this.travelTime = travelTime;
+	}
+
+	public boolean isReachedDestination() {
+		return reachedDestination;
+	}
+
+	public void setReachedDestination(boolean reachedDestination) {
+		this.reachedDestination = reachedDestination;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+
 }
--- src/com/unimelb/swen30006/metromadness/stations/ActiveStation.java
+++ src/com/unimelb/swen30006/metromadness/stations/ActiveStation.java
@@ -17,74 +17,76 @@ import com.unimelb.swen30006.metromadness.trains.Train;
 public class ActiveStation extends Station {
 	// Logger
 	private static Logger logger = LogManager.getLogger();
-	
+
 	public PassengerGenerator g;
 	public ArrayList<Passenger> waiting;
 	public float maxVolume;
-	
+
 	public ActiveStation(float x, float y, PassengerRouter router, String name, float maxPax) {
 		super(x, y, router, name);
 		this.waiting = new ArrayList<Passenger>();
 		this.g = new PassengerGenerator(this, this.lines, maxPax);
 		this.maxVolume = maxPax;
 	}
-	
+
 	@Override
 	public void enter(Train t) throws Exception {
-		if(trains.size() >= PLATFORMS){
+		if (trains.size() >= PLATFORMS) {
 			throw new Exception();
 		} else {
 			// Add the train
 			this.trains.add(t);
 			// Add the waiting passengers
 			Iterator<Passenger> pIter = this.waiting.iterator();
-			while(pIter.hasNext()){
+			while (pIter.hasNext()) {
 				Passenger p = pIter.next();
 				try {
-					logger.info("Passenger "+p.id+" carrying "+p.getCargo().getWeight() +" kg cargo embarking at "+this.name+" heading to "+p.destination.name);
+					logger.info("Passenger " + p.id + " carrying " + p.getCargo().getWeight()
+							+ " kg cargo embarking at " + this.name + " heading to " + p.getDestination().name);
 					t.embark(p);
 					pIter.remove();
-				} catch (Exception e){
+				} catch (Exception e) {
 					// Do nothing, already waiting
 					break;
 				}
 			}
-			
-			//Do not add new passengers if there are too many already
-			if (this.waiting.size() > maxVolume){
+
+			// Do not add new passengers if there are too many already
+			if (this.waiting.size() > maxVolume) {
 				return;
 			}
 			// Add the new passenger
 			Passenger[] ps = this.g.generatePassengers();
-			for(Passenger p: ps){
+			for (Passenger p : ps) {
 				try {
-					logger.info("Passenger "+p.id+" carrying "+p.getCargo().getWeight() +" kg embarking at "+this.name+" heading to "+p.destination.name);
+					logger.info("Passenger " + p.id + " carrying " + p.getCargo().getWeight() + " kg embarking at "
+							+ this.name + " heading to " + p.getDestination().name);
 					t.embark(p);
-				} catch(Exception e){
+				} catch (Exception e) {
 					this.waiting.add(p);
 				}
 			}
 		}
 	}
-	
-	public void render(ShapeRenderer renderer){
+
+	public void render(ShapeRenderer renderer) {
 		float radius = RADIUS;
-		for(int i=0; (i<this.lines.size() && i<MAX_LINES); i++){
+		for (int i = 0; (i < this.lines.size() && i < MAX_LINES); i++) {
 			Line l = this.lines.get(i);
 			renderer.setColor(l.lineColour);
 			renderer.circle(this.position.x, this.position.y, radius, NUM_CIRCLE_STATMENTS);
 			radius = radius - 1;
 		}
-		
+
 		// Calculate the percentage
-		float t = this.trains.size()/(float)PLATFORMS;
+		float t = this.trains.size() / (float) PLATFORMS;
 		Color c = Color.WHITE.cpy().lerp(Color.DARK_GRAY, t);
-		if(this.waiting.size() > 0){
+		if (this.waiting.size() > 0) {
 			c = Color.RED;
 		}
-		
+
 		renderer.setColor(c);
-		renderer.circle(this.position.x, this.position.y, radius, NUM_CIRCLE_STATMENTS);		
+		renderer.circle(this.position.x, this.position.y, radius, NUM_CIRCLE_STATMENTS);
 	}
 
 }
